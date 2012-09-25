a217942198e85c9ae2be8e3bafcaba6df7a2f57a
Francisco Trindade <frank.trindade@gmail.com>
2012-09-25 21:49:52 +1000

    commiting so I can try something new here

--- lib/stomping_ground/server.rb
+++ lib/stomping_ground/server.rb
@@ -10,13 +10,26 @@ module StompingGround
     end
 
     def start
-      server = TCPServer.new(@host, @port)
-      client = server.accept
-      frame = client.readline
-      client.write "CONNECTED\n"
-      client.write "version:1.1\n"
-      client.write "\n"
-      client.write "\0"
+      @server = TCPServer.new(@host, @port)
+      puts "server really started"
+      client = @server.accept
+      puts "accepted client"
+      while frame = client.readline do
+        break if frame == "\0"
+        puts frame
+        if frame =~ /CONNECT/
+          client.write "CONNECTED\n"
+          client.write "version:1.1\n"
+          client.write "\n"
+          client.write "\0"
+          client.close
+        elsif frame =~ /DISCONNECT/
+          client.write "RECEIPT\n"
+          client.write "receipt-id:99\n"
+          client.write "\0"
+          client.close
+        end
+      end
     end
 
   end
--- spec/lib/stomping_ground/server_spec.rb
+++ spec/lib/stomping_ground/server_spec.rb
@@ -5,13 +5,47 @@ describe StompingGround do
   let(:stomp_uri) { 'stomp://127.0.0.1:2000' }
 
   it "should allow client to connect" do
-    Thread.new do
+    server_thread = Thread.new do
       StompingGround::Server.new('127.0.0.1','2000').start
     end
 
     client = OnStomp::Client.new("stomp://127.0.0.1:2000")
     client.connect
     client.connected?.should be_true
+    client.disconnect
+
+    server_thread.kill
+  end
+
+  it "should allow client to disconnect" do
+    puts "starting server"
+    server_thread = Thread.new do
+      StompingGround::Server.new('127.0.0.1','2000').start
+    end
+    puts "server started"
+
+    client = OnStomp::Client.new("stomp://127.0.0.1:2000")
+    puts "client created"
+    client.connect
+    puts "client connected"
+    client.disconnect
+    client.connected?.should be_false
+
+    server_thread.terminate
+  end
+
+  it "should allow client to subscribe" do
+    server_thread = Thread.new do
+      StompingGround::Server.new('127.0.0.1','2000').start
+    end
+
+    client = OnStomp::Client.new("stomp://127.0.0.1:2000")
+    client.connect
+    client.subscribe("queue", :ack => 'client') do |message|
+    end
+    client.disconnect
+
+    server_thread.terminate
   end
 
 end
