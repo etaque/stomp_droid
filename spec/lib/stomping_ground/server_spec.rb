b746160b6ef06051c0f942451f36f809fd2f4687
Francisco Trindade <frank.trindade@gmail.com>
2012-09-26 15:37:14 +1000

    some refactoring

--- spec/lib/stomping_ground/server_spec.rb
+++ spec/lib/stomping_ground/server_spec.rb
@@ -4,32 +4,34 @@ describe StompingGround do
 
   let(:stomp_uri) { 'stomp://127.0.0.1:2000' }
 
-  it "should allow client to connect and disconnect" do
-    server_thread = Thread.new do
+  before :each do
+    @server_thread = Thread.new do
       StompingGround::Server.new('127.0.0.1','2000').start
     end
+  end
 
+  after :each do
+    @server_thread.terminate
+  end
+
+  it "should allow client to connect and disconnect" do
     client = OnStomp::Client.new("stomp://127.0.0.1:2000")
     client.connect
     client.connected?.should be_true
     client.disconnect
     client.connected?.should be_false 
-
-    server_thread.terminate
   end
 
   it "should allow client to subscribe" do
-    server_thread = Thread.new do
-      StompingGround::Server.new('127.0.0.1','2000').start
-    end
-
     client = OnStomp::Client.new("stomp://127.0.0.1:2000")
     client.connect
     client.subscribe("queue", :ack => 'client') do |message|
     end
     client.disconnect
+  end
 
-    server_thread.terminate
+  it "should send specified message when client subscribes" do
+    pending
   end
 
 end
